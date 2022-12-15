f2f885cdacfbcaa38e0083b83a043380c1b997cd
lamzuong <lamdavid821@gmail.com>
2022-12-15 15:40:13 +0700

    Khung tiktok

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       <div className="App">
         <Routes>
           {publicRoutes.map((route, index) => {
-            // const Layout = route.layout === null ? Fragment : DefaultLayout;
             const Page = route.component;
             let Layout = DefaultLayout;
             if (route.layout) {
