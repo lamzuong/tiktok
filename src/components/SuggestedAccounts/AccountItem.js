888e3237a5063816ffb29a048a15d11193066b66
lamzuong <lamdavid821@gmail.com>
2022-12-18 19:48:38 +0700

    sidebar_v2

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './SuggestedAccounts.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('account-item')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1671534000&x-signature=IGpwJ74MmZf%2B5BF56Y2suR9ndl0%3D"
+        alt="Avatar"
+      />
+      <div className={cx('item-info')}>
+        <p className={cx('nickname')}>
+          <strong>nguyn.avan</strong>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </p>
+        <p className={cx('name')}>Nguyen Van A</p>
+      </div>
+    </div>
+  );
+}
+
+AccountItem.propTypes = {};
+
+export default AccountItem;
